32d37418349b77bfdeee426143b5f6b195fa2a77
Reece <reecemorgandev@gmail.com>
2025-07-02 17:14:10 +0100

    implemented main menu

--- scenes/menus/title_screen.gd
+++ scenes/menus/title_screen.gd
@@ -4,17 +4,10 @@ extends Control
 @export var next_scene: String
 
 
-@onready var high_score_label: Label = $CanvasLayer/HighScoreLabel
-
-
 func _ready() -> void:
 	var bg_music = GameManager.get_node("MusicPlayer")
 	if bg_music and not bg_music.playing:
 		bg_music.play() 
-	GameManager.high_score = GameManager.load_high_score()
-	if GameManager.high_score <= 0:
-		high_score_label.visible = false
-	high_score_label.text = "High Score: %d" % GameManager.high_score
 
 
 func _process(_delta: float) -> void:
