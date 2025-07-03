dd624b445930047e32823230e5737af3099d900b
Reece <reecemorgandev@gmail.com>
2025-07-03 16:03:33 +0100

    fixed bug with UI after quitting from pause menu

--- scenes/menus/pause_screen.gd
+++ scenes/menus/pause_screen.gd
@@ -24,4 +24,6 @@ func _process(_delta: float) -> void:
 		get_tree().paused = false
 	
 	if Input.is_action_just_pressed("quit"):
+		canvas_layer.visible = false
+		get_tree().paused = false
 		get_tree().change_scene_to_file.call_deferred(main_menu_scene)
