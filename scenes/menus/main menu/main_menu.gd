14026ba93ec69c274b591c3a59a096077cd45666
Reece <reecemorgandev@gmail.com>
2025-07-03 10:19:06 +0100

    created options and high scores menu

--- scenes/menus/main menu/main_menu.gd
+++ scenes/menus/main menu/main_menu.gd
@@ -3,18 +3,13 @@ extends Control
 
 @export var game_scene: String
 @export var options_scene: String
+@export var high_score_scene: String
 
 @onready var start_button: Button = $CanvasLayer/VBoxContainer/StartButton
-@onready var high_score_label: Label = $CanvasLayer/HighScoreLabel
 
 
 func _ready() -> void:
 	start_button.grab_focus()
-	
-	GameManager.high_score = GameManager.load_high_score()
-	if GameManager.high_score <= 0:
-		high_score_label.visible = false
-	high_score_label.text = "High Score: %d" % GameManager.high_score
 
 
 func _on_start_button_pressed() -> void:
@@ -23,3 +18,7 @@ func _on_start_button_pressed() -> void:
 
 func _on_options_button_pressed() -> void:
 	get_tree().change_scene_to_file.call_deferred(options_scene)
+
+
+func _on_high_score_button_pressed() -> void:
+	get_tree().change_scene_to_file.call_deferred(high_score_scene)
---
+++ scenes/menus/options/options.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+@export var prev_scene: String
+
+
+@onready var back_button: Button = $CanvasLayer/BackButton
+
+
+func _ready() -> void:
+	back_button.grab_focus()
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file.call_deferred(prev_scene)
