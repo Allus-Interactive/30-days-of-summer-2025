1e3594cde55d38ec3517384f0dd11db067675526
Reece <reecemorgandev@gmail.com>
2025-07-03 10:35:39 +0100

    updated pause menu logic

--- scenes/menus/pause_screen.gd
+++ scenes/menus/pause_screen.gd
@@ -16,18 +16,13 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
-		if is_paused:
-			is_paused = false
-			canvas_layer.visible = false
-			get_tree().paused = false
-		else:
-			is_paused = true
-			canvas_layer.visible = true
-			get_tree().paused = true
+		canvas_layer.visible = true
+		get_tree().paused = true
+	
+	if Input.is_action_just_pressed("start"):
+		canvas_layer.visible = false
+		get_tree().paused = false
+		
 	
 	if Input.is_action_just_pressed("quit"):
-			is_paused = true
-			canvas_layer.visible = true
-			get_tree().paused = true
-			
-			get_tree().change_scene_to_file.call_deferred(main_menu_scene)
+		get_tree().change_scene_to_file.call_deferred(main_menu_scene)
