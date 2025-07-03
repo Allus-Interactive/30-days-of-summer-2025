04c46f9975db40aa3964338c2469cc920b09dbd1
Reece <reecemorgandev@gmail.com>
2025-07-03 11:03:14 +0100

    implemented mute/unmute functionality

--- scenes/menus/pause_screen.gd
+++ scenes/menus/pause_screen.gd
@@ -22,7 +22,6 @@ func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("start"):
 		canvas_layer.visible = false
 		get_tree().paused = false
-		
 	
 	if Input.is_action_just_pressed("quit"):
 		get_tree().change_scene_to_file.call_deferred(main_menu_scene)
