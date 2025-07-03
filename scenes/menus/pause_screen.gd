dec4a9317b5412f6ed083b579df552f9794a73f8
Reece <reecemorgandev@gmail.com>
2025-07-03 10:26:08 +0100

    updated pause menu

--- scenes/menus/pause_screen.gd
+++ scenes/menus/pause_screen.gd
@@ -4,6 +4,9 @@ extends Control
 var is_paused: bool = false
 
 
+@export var main_menu_scene: String
+
+
 @onready var canvas_layer: CanvasLayer = $CanvasLayer
 
 
@@ -21,3 +24,10 @@ func _process(_delta: float) -> void:
 			is_paused = true
 			canvas_layer.visible = true
 			get_tree().paused = true
+	
+	if Input.is_action_just_pressed("quit"):
+			is_paused = true
+			canvas_layer.visible = true
+			get_tree().paused = true
+			
+			get_tree().change_scene_to_file.call_deferred(main_menu_scene)
