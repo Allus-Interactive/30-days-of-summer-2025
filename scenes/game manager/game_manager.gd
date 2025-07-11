b5b40ac12f5379b89f421e1c5070539fd67801dd
Reece <reecemorgandev@gmail.com>
2025-07-11 09:51:19 +0100

    reworked menus and created local leaderboard

--- scenes/game manager/game_manager.gd
+++ scenes/game manager/game_manager.gd
@@ -7,6 +7,21 @@ var cooking_streak: int = 0
 var is_muted: bool = false
 signal flip_the_food(current_position: float)
 
+var leaderboard = {
+	"scores": [  # Each entry can be a dictionary with name and score
+		{"name": "LFC", "score": 200},
+		{"name": "GOD", "score": 180},
+		{"name": "PRO", "score": 160},
+		{"name": "ABC", "score": 140},
+		{"name": "DEF", "score": 120},
+		{"name": "GHI", "score": 100},
+		{"name": "JKL", "score": 80},
+		{"name": "MNO", "score": 60},
+		{"name": "PQR", "score": 40},
+		{"name": "STV", "score": 20},
+	]
+}
+
 
 func save_high_score(score: int) -> void:
 	var file = FileAccess.open("user://highscore.save", FileAccess.WRITE)
@@ -47,3 +62,27 @@ func _on_request_completed(_result: int, response_code: int, _headers: PackedStr
 		print("Score submitted successfully!")
 	else:
 		print("Failed to submit score. Response code:", response_code)
+
+
+func save_local_leaderboard():
+	var file = FileAccess.open("user://leaderboard.json", FileAccess.WRITE)
+	if file:
+		var json = JSON.stringify(leaderboard, "\t")
+		file.store_string(json)
+		file.close()
+
+
+func load_local_leaderboard():
+	if not FileAccess.file_exists("user://leaderboard.json"):
+		print("No leaderboard file found.")
+		return
+	
+	var file = FileAccess.open("user://leaderboard.json", FileAccess.READ)
+	if file:
+		var content = file.get_as_text()
+		var result = JSON.parse_string(content)
+		if result is Dictionary:
+			leaderboard = result
+		else:
+			print("Error parsing leaderboard data.")
+		file.close()
--- scenes/menus/high scores/high_scores.gd
+++ scenes/menus/high scores/high_scores.gd
@@ -1,47 +1,23 @@
-extends Control
+extends Node2D
 
 
 @export var prev_scene: String
 
 
-@onready var high_score_label: Label = $CanvasLayer/HBoxContainer/VBoxContainer/HighScoreLabel
-@onready var online_high_score_label: Label = $CanvasLayer/HBoxContainer/VBoxContainer2/OnlineHighScoreLabel
+@onready var local_leaderboard_container: VBoxContainer = $CanvasLayer/HBoxContainer/LocalLeaderboardContainer
 @onready var back_button: Button = $CanvasLayer/BackButton
 
 
 func _ready() -> void:
 	back_button.grab_focus()
-	
-	GameManager.high_score = GameManager.load_high_score()
-	if GameManager.high_score <= 0:
-		high_score_label.visible = false
-	high_score_label.text = "Your High\nScore: %d" % GameManager.high_score
-	
-	# TODO: implement online High Score Leaderboard
-	# online_high_score_label.text = ""
-	# get_high_scores()
+	GameManager.load_local_leaderboard()
+	var label = Label.new()
+	label.theme = preload("res://assets/themes/leaderboard.tres")
+	for i in GameManager.leaderboard["scores"].size():
+		var entry = GameManager.leaderboard["scores"][i]
+		label.text += str(i + 1) + ": " + entry["name"] + " - " + str(entry["score"]) + "\n"
+		local_leaderboard_container.add_child(label)
 
-
-func get_high_scores():
-	var http = HTTPRequest.new()
-	add_child(http)
-	http.request_completed.connect(_on_scores_received)
-	http.request("http://localhost:3000/scores")
-
-
-func _on_scores_received(_result, _code, _headers, body):
-	var json = JSON.new()
-	var error = json.parse(body.get_string_from_utf8())
-	
-	if error == OK:
-		var result = json.data
-		print(result)
-		for entry in result:
-			print("%s: %d\n" % [entry["name"], entry["score"]])
-			online_high_score_label.text += "%s: %d\n" % [entry["name"], entry["score"]]
-	else:
-		print("JSON parsing failed with error code: ", error)
-		online_high_score_label.text = "Unable to retrieve high scores. Please try again later."
 
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file.call_deferred(prev_scene)
