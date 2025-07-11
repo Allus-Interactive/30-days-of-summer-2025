1769838014ddf483a8096c3212bf36f6b41774c3
Reece <reecemorgandev@gmail.com>
2025-07-11 10:17:08 +0100

    added global lb logic to new scene

--- scenes/menus/high scores/high_scores.gd
+++ scenes/menus/high scores/high_scores.gd
@@ -5,18 +5,52 @@ extends Node2D
 
 
 @onready var local_leaderboard_container: VBoxContainer = $CanvasLayer/HBoxContainer/LocalLeaderboardContainer
+@onready var local_leaderboard: Label = $CanvasLayer/HBoxContainer/LocalLeaderboardContainer/LocalLeaderboard
+@onready var global_leaderboard: Label = $CanvasLayer/HBoxContainer/OnlineLeaderboardContainer/GlobalLeaderboard
 @onready var back_button: Button = $CanvasLayer/BackButton
+@onready var global_title: Label = $CanvasLayer/HBoxContainer/OnlineLeaderboardContainer/GlobalTitle
 
 
 func _ready() -> void:
 	back_button.grab_focus()
+	
+	# Local Leaderboard Logic
 	GameManager.load_local_leaderboard()
-	var label = Label.new()
-	label.theme = preload("res://assets/themes/leaderboard.tres")
 	for i in GameManager.leaderboard["scores"].size():
 		var entry = GameManager.leaderboard["scores"][i]
-		label.text += str(i + 1) + ": " + entry["name"] + " - " + str(entry["score"]) + "\n"
-		local_leaderboard_container.add_child(label)
+		local_leaderboard.text += str(i + 1) + ": " + entry["name"] + " - " + str(entry["score"]) + "\n"
+	
+	# Global Leaderboard Logic
+	# TODO: implement online High Score Leaderboard
+	# get_high_scores()
+
+
+func get_high_scores():
+	print("Retreiving global leaderboard...")
+	var http = HTTPRequest.new()
+	# add request node to scene tree
+	add_child(http)
+	# connect function to run on request completion
+	http.request_completed.connect(_on_scores_received)
+	# make the request
+	http.request("http://localhost:3000/scores")
+
+
+func _on_scores_received(_result, _code, _headers, body):
+	
+	var json = JSON.new()
+	var error = json.parse(body.get_string_from_utf8())
+	
+	if error == OK:
+		var result = json.data
+		print(result)
+		for entry in result:
+			print("%s: %d\n" % [entry["name"], entry["score"]])
+			global_leaderboard.text += "%s: %d\n" % [entry["name"], entry["score"]]
+	else:
+		print("JSON parsing failed with error code: ", error)
+		global_title.visible = false
+		global_leaderboard.text = "Unable to retrieve leaderboard. Please try again later."
 
 
 func _on_back_button_pressed() -> void:
