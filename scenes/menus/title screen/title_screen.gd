799075da43a869d4a3f5458c0128a5c6948acb59
Reece <reecemorgandev@gmail.com>
2025-07-25 15:21:11 +0100

    added title screen sfx

--- scenes/menus/title screen/title_screen.gd
+++ scenes/menus/title screen/title_screen.gd
@@ -4,10 +4,14 @@ extends Control
 @export var next_scene: String
 
 
+@onready var title_sfx: AudioStreamPlayer2D = $TitleSFX
+
+
 func _ready() -> void:
 	var bg_music = GameManager.get_node("MusicPlayer")
 	if bg_music and not bg_music.playing:
-		bg_music.play() 
+		bg_music.play()
+	title_sfx.play() 
 
 
 func _process(_delta: float) -> void:
